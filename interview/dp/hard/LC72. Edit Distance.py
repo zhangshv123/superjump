c2f44c426ab2dbdb7d088e44899b5034ae4ced96
Shuhan Zhang <sz1266@nyu.edu>
2018-06-24 15:26:26 -0700

    added

--- interview/dp/hard/LC72. Edit Distance.py
+++ interview/dp/hard/LC72. Edit Distance.py
@@ -9,6 +9,7 @@ c) Replace a character
 """
 """
 f(i, j) := minimum cost (or steps) required to convert first i characters of word1 to first j characters of word2
+这里是前i个和前j个，为了代码便于初始化而已
 
 Case 1: word1[i] == word2[j], i.e. the ith the jth character matches.
 
@@ -22,38 +23,30 @@ f(i - 1, j - 1) represents replace operation
 """
 #bottom-up translation of this recurrent relation
 转移方程是：
-if word1[i - 1] == word2[j - 1]:
-    dp[i][j] = dp[i - 1][j - 1]
-else:
-    #replace, delete, insert
-    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+dp[i][j] = 三个之中找最小值 min(
+    dp[i-1][j] + 1, delete
+    dp[i][j-1] + 1, insert
+    dp[i-1][j-1] if word1[i] == word2[j] 如果不等 dp[i-1][j-1]+1) replace
+
+import sys
 class Solution(object):
     def minDistance(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: int
-        """
-        m, n = len(word1), len(word2)
-        if m == 0 or n == 0:
-            return max(m, n)
-        #dp[i][j]: word1[:i] 转化为 word2[:j] 子问题
-        dp = [[sys.maxint for _ in range(n + 1)] for _ in range(m + 1)] #多一位0代表不存在任何char，而不是第0位的
-        for i in range(m + 1):
-            for j in range(n + 1):
-                #init
-                if i == 0:
-                    dp[i][j] = j
-                elif j == 0:
-                    dp[i][j] = i
+        dp = [[0 for _ in range(len(word2) + 1)] for _ in range(len(word1) + 1)] #这里初始化为0，初始化为任何数都可以！
+        dp[0][0] = 0
+        for j in range(len(word2) + 1):
+            dp[0][j] = j
+        for i in range(len(word1) + 1):
+            dp[i][0] = i
+        for i in range(1,len(word1)+1):
+            for j in range(1,len(word2)+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
                 else:
-                    #dp function
-                    if word1[i - 1] == word2[j - 1]:
-                        dp[i][j] = dp[i - 1][j - 1]
-                    else:
-                        #replace, delete, insert
-                        dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
-        return dp[m][n]
+                    dp[i][j] = dp[i-1][j-1]+1
+                
+                dp[i][j] = min(dp[i][j-1]+1,dp[i-1][j]+1,dp[i][j])
+        return dp[-1][-1]
+
 #滚动数组        
 class Solution(object):
     def minDistance(self, word1, word2):
