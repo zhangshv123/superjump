edd7cab220ba58f07aedffb1e16d4bac54e1a513
“shuhan <zhangshuhan@gmail.com>
2018-07-01 16:00:10 -0700

    added

--- template/11_kMerge.py
+++ template/11_kMerge.py
@@ -1,4 +1,5 @@
 import heapq
+n个array,每个array有k个num
 Time:0(kn)因为每个element用一次
 def kMerge(k,arrs):
 	h = []
@@ -22,10 +23,15 @@ def kMerge(k,arrs):
 
 print kMerge(3,[[1, 3, 5, 7],[2, 4, 6, 8],[0, 9, 10, 11]])
 1.merge2个复杂度多少？k个多少？
-
+knlog(n)
 2.比较优缺点
-
+merge2: 浪费临时存储空间
+每次中间的临时都要存储到disk里面去，然后还要读出来，就慢很多
+kmerge:节省空间
+	
 3.k个array 在k个文件里，每个文件4G，memery2个G（外部排序）
+kmerge,每次merge 2G
+每个文件读2G/K，好要留点给排好序的部分
 
 		
 	
