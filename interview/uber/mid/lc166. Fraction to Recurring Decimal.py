409ddbc7fc2a89432541801bcd22281b950a46f7
Shuhan Zhang <sz1266@nyu.edu>
2018-07-12 21:52:16 -0700

    added

--- interview/uber/mid/lc166. Fraction to Recurring Decimal.py
+++ interview/uber/mid/lc166. Fraction to Recurring Decimal.py
@@ -1,35 +1,43 @@
 解题思路：
-class Solution:
-	# @return a string
+请参考
+http://tinyurl.com/y9jkbglj
+class Solution(object):
 	def fractionToDecimal(self, numerator, denominator):
-		negativeFlag = numerator * denominator < 0
+		if denominator == 0:
+			return 0
+		negative = numerator*denominator < 0
 		numerator = abs(numerator)
 		denominator = abs(denominator)
-		numList = []
+		loopArr = []
+		d = dict()
+		res = None
 		cnt = 0
-		loopDict = dict()
-		loopStr = None
 		while True:
-			numList.append(str(numerator / denominator))
 			cnt += 1
-			numerator = 10 * (numerator % denominator)
-			if numerator == 0:
+			inte = numerator/denominator
+			left = (numerator%denominator)*10
+			loopArr.append(str(inte))
+			numerator = left
+			if left == 0:
+				if len(loopArr) > 1:
+					res =loopArr[0] + "."+ "".join(loopArr[1:])
+					break
+				else:
+					res = loopArr[0]
+					break
+			if left in d:
+				start = d[left]
+				res = str(loopArr[0]) + "." + "".join(loopArr[1:start]) + "(" + "".join(loopArr[start:]) + ")"
 				break
-			loc = loopDict.get(numerator)
-			if loc:
-				loopStr = "".join(numList[loc:cnt])
-				break
-			loopDict[numerator] = cnt
-		ans = numList[0]
-		if len(numList) > 1:
-			ans += "."
-		if loopStr:
-			ans += "".join(numList[1:len(numList) - len(loopStr)]) + "(" + loopStr + ")"
-		else:
-			ans += "".join(numList[1:])
-		if negativeFlag:
-			ans = "-" + ans
-		return ans
+			else:
+				d[left] = cnt
+		if negative:
+			res  = "-"+res
+		return res
 
 s = Solution()
-print s.fractionToDecimal(50, 22)
+print s.fractionToDecimal(-2,-6)
+				
+		
+			
+	
