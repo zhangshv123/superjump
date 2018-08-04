d74e5a08e970f9f969c6d9eef66789be819a6c76
“shuhan <zhangshuhan@gmail.com>
2018-08-04 15:19:21 -0700

    added

---
+++ template/01bruteforce_template.py
@@ -0,0 +1,20 @@
+代表有n个苹果，每次可以最多拿n个，返回所有的可能性
+比如：n = 2
+返回 [[0, 0], [0, 1], [1, 0], [1, 1]]
+def printN(n):
+	res = []
+	dfs(n,0,res, [],[0,1])
+	return res
+
+def dfs(n, idx, res, path, possible):
+	if idx == n:
+		res.append(path[:])
+		return
+	
+	for pos in possible:
+		path.append(pos)
+		dfs(n, idx+1, res, path, possible)
+		path.pop()
+
+print printN(2)
+	
