757fee014f77d314d313f7e39992335255e22139
“shuhan <zhangshuhan@gmail.com>
2018-08-19 15:14:25 -0700

    added

--- shuhan/facebook/shuhan/mid/lc314-Binary Tree Vertical Order Traversal.py
+++ shuhan/facebook/shuhan/mid/lc314-Binary Tree Vertical Order Traversal.py
@@ -2,24 +2,25 @@
 from collections import defaultdict
 from collections import deque
 class Solution(object):
+	class Solution(object):
 	def verticalOrder(self, root):
-#		这里把defaultdict的value设定为list类型
-		vertical_map = defaultdict(list)
-		if root is not None:
-			self.bfs(root,vertical_map)
-		res_list = []
-		for key, value in sorted(vertical_map.items()): #注意vertical_map.items()返回一个tuple(A,B),然后按照A来排序
-			res_list.append(value)
-		return res_list
-	
-	def bfs(self,root,vetical_map):
-		queue = deque()
-		queue.append((0,root))
-		while len(queue)>0:
-			cur = queue.popleft()
-			index,node = cur[0],cur[1]
-			vetical_map[index].append(node.val)
-			if node.left:
-				queue.append((index-1,node.left))
-			if node.right:
-				queue.append((index+1,node.right))
+		res = []
+		if not root:
+			return res
+		d = defaultdict(list)
+		q = deque()
+		q.append((root, 0))
+		while len(q) > 0:
+			size = len(q)
+			for i in range(size):
+				cur,idx = q.popleft()
+				d[idx].append(cur.val)
+				if cur.left:
+					q.append((cur.left, idx-1))
+				if cur.right:
+					q.append((cur.right, idx+1))
+		keys = sorted(d.keys())
+		for key in keys:
+			res.append(d[key])
+		return res
+
