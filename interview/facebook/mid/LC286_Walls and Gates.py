3c459c6ae1a622350e917005db6beeb48f90d290
“shuhan <zhangshuhan@gmail.com>
2018-08-24 17:09:28 -0700

    added

--- interview/facebook/mid/LC286_Walls and Gates.py
+++ interview/facebook/mid/LC286_Walls and Gates.py
@@ -1,46 +1,30 @@
-#!/usr/bin/python
-"""
-You are given a m x n 2D grid initialized with these three possible values.
-
--1 - A wall or an obstacle.
-0 - A gate.
-INF - Infinity means an empty room. We use the value 231 - 1 = 2147483647 to represent INF as you may assume that the distance to a gate is less than 2147483647.
-Fill each empty room with the distance to its nearest gate. If it is impossible to reach a gate, it should be filled with INF.
-
-For example, given the 2D grid:
-INF  -1  0  INF
-INF INF INF  -1
-INF  -1 INF  -1
-    0  -1 INF INF
-After running your function, the 2D grid should be:
-    3  -1   0   1
-    2   2   1  -1
-    1  -1   2  -1
-    0  -1   3   4
-"""
-"""
-top bfs
-"""
+思路：
+我们可以每个“INF”点做BFS，但是那样会超时，所以我们就需要用更巧妙的方法！
+我们先找到所有0的位置，然后把这些点放进queue，然后从这些0开始向外找！
 from collections import deque
 class Solution(object):
     def wallsAndGates(self, rooms):
-        """
-        :type rooms: List[List[int]]
-        :rtype: void Do not return anything, modify rooms in-place instead.
-        """
+        if not rooms or len(rooms) == 0 or len(rooms[0]) == 0:
+            return
+        row = len(rooms)
+        col = len(rooms[0])
         q = deque()
-        for i in range(len(rooms)):
-            for j in range(len(rooms[i])):
+        directions = [(-1,0),(0,-1),(1,0),(0,1)]
+        for i in range(row):
+            for j in range(col):
                 if rooms[i][j] == 0:
-                    q.append((i, j))
+                    q.append((i,j))
         while len(q) > 0:
-            i, j = q.pop()
-            for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                ni, nj, ndis = i + di, j + dj, rooms[i][j] + 1
-                if ni >=0 and ni < len(rooms) and nj >= 0 and nj < len(rooms[0]):
-                    if rooms[ni][nj] > ndis:
-                        rooms[ni][nj] = ndis
-                        q.append((ni, nj))
+            i,j = q.popleft()
+            for dire in directions:
+                new_x = i+dire[0] 
+                new_y = j+dire[1]
+                new_d = rooms[i][j]+1
+                if new_x >= 0 and new_x < len(rooms) and new_y >= 0 and new_y < len(rooms[0]):
+                    if rooms[new_x][new_y] > new_d:
+                        rooms[new_x][new_y] = new_d
+                        q.append((new_x,new_y))
+        return
 
 """
 naive dfs
