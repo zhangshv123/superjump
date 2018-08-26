6eb96b0965beadccc4ca70979e7c2294cd21e15b
“shuhan <zhangshuhan@gmail.com>
2018-08-26 16:16:44 -0700

    added

--- template/4_N-Queens.py
+++ template/4_N-Queens.py
@@ -2,20 +2,27 @@
 class Solution(object):
 	def solveNQueens(self, n):
 		res = []
-		self.dfs(n,res,[],0)
+		self.dfs(0, n, [], res)
 		return res
 	
-	def dfs(self, n, res, path, row):
+	def dfs(self, row, n, path, res):
 		if len(path) == n:
 			res.append(path[:])
 			return
 			
-		for i in range(n):
-			if isValid(path,row,i): #合理才会放进去 
-				path.append((row,i))
-				self.dfs(n, res, path, row+1)
+		for j in range(n):
+			if isValid(path, row, j): #合理才会放进去 
+				path.append((row,j))
+				self.dfs(row+1, n, path, res)
 				path.pop()
-
+	
+	def isValid(self, row, col, path): #行不用检查，只需要检查之前行的同列
+		for point in path:
+			if point[1] == col:
+				return False
+			if abs(row - point[0]) == abs(col - point[1]): #检查斜线
+				return False
+		return True
 			
 			
 			
