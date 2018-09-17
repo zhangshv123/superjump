f8410f353196c8de1aebaf13b54464efa3d59cb7
“shuhan <zhangshuhan@gmail.com>
2018-09-17 16:19:11 -0700

    added

--- interview/google/hard/LC295. Find Median from Data Stream.py
+++ interview/google/hard/LC295. Find Median from Data Stream.py
@@ -19,6 +19,9 @@ findMedian() -> 1.5
 addNum(3) 
 findMedian() -> 2
 """
+思路：
+可以选用一个最大堆一个最小堆，保持两个堆的大小平衡，让大顶堆保存小的一半的数，小顶堆保存较大的一半数．
+import heapq
 class MedianFinder(object):
 
     def __init__(self):
@@ -27,27 +30,25 @@ class MedianFinder(object):
         """
         #assume minHeap could at most be larger than maxHeap by 1
         self.maxHeap, self.minHeap = [], []
-        self.isEven = True
 
     def addNum(self, num):
         """
         :type num: int
         :rtype: void
         """
-        if self.isEven:
-            heapq.heappush(self.maxHeap, -num)
+        heapq.heappush(self.minHeap, num) #一定要先放minheap，这样2边的大小size diff不会超过1
+        heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
+        
+        if len(self.minHeap) < len(self.maxHeap):
             heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
-        else:
-            heapq.heappush(self.minHeap, num)
-            heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
-        self.isEven = not self.isEven 
-
+        
     def findMedian(self):
         """
         :rtype: float
         """
         # print self.minHeap, self.maxHeap, self.isEven
-        if self.isEven:
+        size = len(self.minHeap) + len(self.maxHeap)
+        if size % 2 == 0:
             return (self.minHeap[0] - self.maxHeap[0]) / 2.0
         else:
             return self.minHeap[0]
