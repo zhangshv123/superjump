39199541a0525d447ce53a6ed9c4533ee20ea080
“shuhan <zhangshuhan@gmail.com>
2018-09-23 16:14:21 -0700

    added

---
+++ template/15_quicksort.py
@@ -0,0 +1,20 @@
+思路：参考geeksforgeeks
+https://www.geeksforgeeks.org/quick-sort/
+def quicksort(arr, low, high):
+	if low < high:
+		pi = partition(arr, low, high)
+		quicksort(arr, low, pi-1)
+		quicksort(arr, pi+1, high)
+	return arr
+
+def partition(arr, low, high):
+	pivot = arr[high]
+	i = low - 1
+	for j in range(low, high):
+		if arr[j] <= pivot:
+			i += 1
+			arr[i], arr[j] = arr[j], arr[i]
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return i+1
+
+print quicksort([1,4,3,2,7], 0, 4)
