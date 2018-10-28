412021f6505d8497f8b8c29d97cc388e9a049f78
“shuhan <zhangshuhan@gmail.com>
2018-10-28 15:27:14 -0700

    added

--- interview/box/inMemoeryCache.py
+++ interview/box/inMemoeryCache.py
@@ -33,14 +33,72 @@ class Solution(object):
 					cache = cur
 					cacheInverted = curInverted
 		return "end"
-				
-				
-				
-				
-				
-				
-					
-					
 					
 s = Solution()
 print s.inMemoeryCache()
+
+方法2：
+from collections import defaultdict
+class Solution:
+	def tryCache(self):
+		cache = defaultdict(str)
+		cacheInverted = defaultdict(int)
+		stack = []
+		while (True):
+			line = raw_input()
+			if line.startswith("end"):
+				return
+			self.helper(line, cache, cacheInverted, stack)
+			
+	
+	def helper(self, line, cache, cacheInverted, stack, rollback_only=False):
+		if line.startswith("set"):
+			arr = line.split()
+			if arr[1] in cache:
+				target = cache[arr[1]]
+				cacheInverted[target] -= 1
+			if len(stack) > 0 and not rollback_only:
+				if arr[1] in cache:
+					stack[-1].append("set " + arr[1] + " " + target)
+				else:
+					stack[-1].append("delete " + arr[1])
+			cache[arr[1]] = arr[2]
+			cacheInverted[arr[2]] += 1
+		elif line.startswith("get"):
+			key = line.split()[1]
+			if key not in cache:
+				print "NULL"
+			else:
+				print cache[key]
+		elif line.startswith("count"):
+			target = line.split()[1]
+			print cacheInverted[target]
+		elif line.startswith("delete"):
+			arr = line.split()
+			target = cache[arr[1]]
+			cacheInverted[target] -= 1
+			if len(stack) > 0 and not rollback_only:
+				stack[-1].append("set " + arr[1] + " " + target)
+			del cache[arr[1]]
+			print "deleted " + str(arr[1])
+		elif line.startswith("begin"):
+			stack.append([])
+		elif line.startswith("commit"):
+			if len(stack) > 0:
+				stack[:] = []
+				return
+			print "no transaction"
+		elif line.startswith("rollback"):
+			if len(stack) == 0:
+				print "no transaction"
+				return
+			cur = stack.pop()
+			for i in range(len(cur)-1, -1, -1):
+				self.helper(cur[i], cache, cacheInverted, stack, rollback_only=True)
+
+s = Solution()
+s.tryCache()
+		
+				
+			
+		
--- interview/rubrik/findBiggestX.py
+++ interview/rubrik/findBiggestX.py
@@ -4,7 +4,7 @@
 以下是2
 1 0 1
 0 1 0
-1 0 1. 一亩-三分-地，独家发布
+1 0 1. 
 以下是3
 1 0 0 0 1
 0 1 0 1 0
@@ -12,6 +12,68 @@
 0 1 0 1 0
 1 0 0 0 1
 大致想法就是遍历每个元素，算四个方向，取最小值是边长，然后找到最大的，follow up是如何加快，可以每次把一个斜边的算完以后存起来，以后要是发现左上和右上已经算过了，就可以直接用了
+1.brute force的方法：
+	就是遍历matrix里面的每个1，然后4个方向去找长度，最后取4个方向里面最短的作为结果，然后res取所有里面最长的即可
+2.用dp优化：
+4个方向我们称为0，1，2，3（顺时针的）
+第一排的元素，0，1方向都必定为0，然后while循环算出2，3方向的长度，存到dp[0][j][2]和dp[0][j][3]里面
+剩下非第一排的元素，都可以根据左上和右上的元素来确定
+
+时间复杂度0 N^2
+
+dp写法：
+class Solution:
+	def findBiggestX(self, matrix):
+		row, col = len(matrix), len(matrix[0])
+		dp = [[[0 for _ in range(4)] for _ in range(col)] for _ in range(row)]
+		res = 0
+		for j in range(col):
+			if matrix[0][j] == 1:
+				res = 1
+				dp[0][j][0] = 0 #0，1方向都必定为0
+				dp[0][j][1] = 0
+				count2 = 0 #这里开始算第二方向
+				startX, startY = 0, j
+				while startX + 1 < len(matrix) and startY - 1 >= 0:
+					count2 += 1
+					startX += 1
+					startY -= 1
+				dp[0][j][2] = count2
+				count3 = 0 #这里开始算第三方向
+				startX, startY = 0, j
+				while startX + 1 < len(matrix) and startY + 1 < len(matrix[0]):
+					count3 += 1
+					startX += 1
+					startY += 1
+				dp[0][j][3] = count3
+		for i in range(1, row):
+			for j in range(col):
+				tmp = 0
+				if matrix[i][j] == 1:
+					if i-1 >= 0 and j-1 >= 0 and matrix[i-1][j-1] == 1:
+						dp[i][j][0] = dp[i-1][j-1][0]+1
+						tmp = dp[i][j][0]
+					if i-1 >=0 and j+1 < len(matrix[0]) and matrix[i-1][j+1] == 1:
+						dp[i][j][1] = dp[i-1][j+1][1]+1
+						tmp = min(tmp,dp[i][j][1])
+					if i-1 >=0 and j+1 < len(matrix[0]) and matrix[i-1][j+1] == 1:
+						dp[i][j][2] = dp[i-1][j+1][2]-1
+						tmp = min(tmp,dp[i][j][2])
+					if i-1 >= 0 and j-1 >= 0 and matrix[i-1][j-1] == 1:
+						dp[i][j][3] = dp[i-1][j-1][3]-1
+						tmp = min(tmp,dp[i][j][3])
+				res = max(res,tmp+1)
+		return res
+
+s = Solution()
+print s.findBiggestX([[0]])
+print s.findBiggestX([[1]])
+print s.findBiggestX([[1, 0, 1],[0, 1, 0],[1, 0, 1]])
+print s.findBiggestX([[1, 0, 1],[0, 0, 0],[1, 0, 1]])
+print s.findBiggestX([[1,0,0,0,1],[0,1,0,1,0],[0,0,1,0,0],[0,1,0,1,0],[1,0,0,0,1]])
+
+
+brute-force的写法：
 class Solution:
 	def findBiggestX(self, matrix):
 		row, col = len(matrix), len(matrix[0])
