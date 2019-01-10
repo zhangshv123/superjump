069658027109b02dee296c75b5208b9db5325c60
marry <sz1266@nyu.edu>
2019-01-10 10:47:14 -0800

    added

---
+++ interview/linkedin/mid/LC323. Number of Connected Components in an Undirected Graph.py
@@ -0,0 +1,26 @@
+class Solution(object):
+	def countComponents(self, n, edges):
+		visited = set()
+		edges.sort()
+		count = 0
+		for edge in edges:
+			if edge[0] not in visited:
+				count += 1
+				visited.add(edge[0])
+				visited.add(edge[1])
+				self.dfs(edges, visited)
+		return count + n - len(visited)
+	
+	def dfs(self, edges, visited):
+		for edge in edges:
+			if edge[0] in visited and edge[1] not in visited:
+				visited.add(edge[1])
+				self.dfs(edges, visited)
+			elif edge[1] in visited and edge[0] not in visited:
+				visited.add(edge[0])
+				self.dfs(edges, visited)
+		return
+		
+				
+			
+			
