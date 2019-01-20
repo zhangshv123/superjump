189a9d392363a22ec1f7d17355798437e4979197
marry <sz1266@nyu.edu>
2019-01-20 14:54:45 -0800

    added

--- interview/facebook/mid/LC247. Strobogrammatic Number II.py
+++ interview/facebook/mid/LC247. Strobogrammatic Number II.py
@@ -1,38 +1,26 @@
+# http://www.cnblogs.com/grandyang/p/5200919.html
 class Solution(object):
 	def findStrobogrammatic(self, n):
-		d = {"1":"1","6":"9","8":"8","9":"6","0":"0"}
-		if n == 0:
+		return self.find(n,n)
+	
+	def find(self, m, n):
+		if m == 0:
 			return [""]
-		if n == 1:
-			return ["0","1","8"]
-		res = []
-		self.dfs(n/2,[],res,d, n)
-		for i, item in enumerate(res):
-			for j in range(len(item)-1, -1, -1):
-				item.append(d[item[j]])
-				res[i] = "".join(item)	
-		if n%2 == 0:
-			return res
-		else:
-			new_res = []
-			for key in ["1","8","0"]:
-				for i, item in enumerate(res):
-					idx = len(item)/2
-					new_item = item[:idx] + key + item[idx:]
-					new_res.append(new_item)
-			return new_res
-			
-	def dfs(self, left, path, res, d, n):
-		if left == 0:
-			res.append(path[:])
-			return
 		
-		for key in d.keys():
-			if left == n/2 and key == "0":
-				continue
-			path.append(key)
-			self.dfs(left-1, path, res, d, n)
-			path.pop()
+		if m == 1:
+			return ["0", "1", "8"]
+		
+		t = self.find(m-2, n)
+		res = []
+		for a in t:
+			if m != n:
+			res.append("0" + a + "0")
+			res.append("1" + a + "1")
+			res.append("6" + a + "9")
+			res.append("8" + a + "8")
+			res.append("9" + a + "6")
+		
+		return res
 	
 s = Solution()
 print s.findStrobogrammatic(3)
